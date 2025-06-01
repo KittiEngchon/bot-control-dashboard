4750debfdc2e381ae21cb28e1cc96f28b7f20079
KittiEngchon <budtung888@gmail.com>
2025-06-02 00:23:43 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MaticPrice from './components/MaticPrice';
+
+function App() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <MaticPrice />
+    </div>
+  );
+}
+
+export default App;
